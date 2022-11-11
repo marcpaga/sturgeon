1e86e222a8e372bc29f627f32004e666d4be89a6
marcpaga <m.pagesgallego@umcutrecht.nl>
2022-11-11 14:06:10 +0100

    update bin build

--- build_nuitka.sh
+++ build_nuitka.sh
@@ -1,3 +1,16 @@
-python3 -m nuitka --standalone sturgeon/main.py --enable-plugin=numpy
+export INCLUDE=/usr/include
+export C_INCLUDE_PATH=/usr/include
+export CPLUS_INCLUDE_PATH=/usr/include
+export LIBRARY_PATH=/usr/lib/x86_64-conda-linux-gnu-ld
 
-python3 -m nuitka --onefile sturgeon/main.py --enable-plugin=numpy
+python3 -m nuitka \
+--standalone \
+--enable-plugin=numpy \
+--include-data-dir=sturgeon/include=include \
+sturgeon/main.py 
+
+python3 -m nuitka \
+--onefile \
+--enable-plugin=numpy \
+--include-data-dir=sturgeon/include=include \
+sturgeon/main.py 
