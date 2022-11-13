aa6b1e56822d415a0940886b5ac42a0b58dcee4d
Marc Pages <marc.pages@live.com>
2022-11-13 13:36:17 +0100

    add file timestamp

--- sturgeon/utils.py
+++ sturgeon/utils.py
@@ -2,6 +2,7 @@ import zipfile
 import logging
 import os
 from pathlib import Path
+import platform
 
 import pandas as pd
 import numpy as np
@@ -130,3 +131,19 @@ def get_model_path(model_name):
 
     return os.path.join(model_dir, model_name + '.zip')
 
+def creation_date(path_to_file):
+    """
+    Try to get the date that a file was created, falling back to when it was
+    last modified if that isn't possible.
+    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    """
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return stat.st_mtime
